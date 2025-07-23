c0e2c6b203cd69b5e2a866b199ee63a7c6210c48
Miguel Angel Correa <miguel.correa@upb.edu.co>
2025-07-22 21:17:52 -0500

    adelante en la tarea

--- Actividad1-ArquitecturaDelComputador/reto/reto1.asm
+++ Actividad1-ArquitecturaDelComputador/reto/reto1.asm
@@ -10,3 +10,33 @@ M=D
 @24
 D=M
 @200
+M=D
+//punto 4
+@100
+D=M
+@15
+D=D-M
+@100
+M=D
+//punto 5
+@0
+D=M
+@1
+D=D+M
+@69
+D=D+A
+@2
+M=D
+//punto 6
+D;JEQ
+//punto 7 
+@100
+0;JMP
+@20
+//punto 8 
+@var1
+D = M
+@var2
+D = D + M
+@var3
+M = D
