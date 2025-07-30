1bcf27bfe13255f5dddb938620d5a000af0d7f37
Miguel Angel Correa <miguel.correa@upb.edu.co>
2025-07-30 17:30:41 -0500

    adelanto de clase

--- Actividad 2/investigacion/actvidad 3.asm
+++ Actividad 2/investigacion/actvidad 3.asm
@@ -1,433 +1,563 @@
 (LOOP)
-    @KBD
-    D=M
-    @NO_KEY
-    D;JEQ
-    @100
-    D=D-A
-    @DRAW
-    D;JEQ
-    @CLEAR
-    0;JMP
-
+@KBD
+D=M
+@NO_KEY
+D;JEQ
+@100
+D=D-A
+@DRAW
+D;JEQ
+@CLEAR
+0;JMP
 (NO_KEY)
-    @CLEAR
-    0;JMP
-
+@CLEAR
+0;JMP
 (CLEAR)
-    @SCREEN
-    D=A
-    @R12
-    M=D
-    @8192
-    D=A
-    @R13
-    M=D
-
+@SCREEN
+D=A
+@R12
+M=D
+@8192
+D=A
+@R13
+M=D
 (CLEAR_LOOP)
-    @R13
-    D=M
-    @CLEAR_DONE
-    D;JEQ
-    @R12
-    A=M
-    M=0
-    @R12
-    M=M+1
-    @R13
-    M=M-1
-    @CLEAR_LOOP
-    0;JMP
-
+@R13
+D=M
+@CLEAR_DONE
+D;JEQ
+@R12
+A=M
+M=0
+@R12
+D=M
+D=D+1
+@R12
+M=D
+@R13
+D=M
+D=D-1
+@R13
+M=D
+@CLEAR_LOOP
+0;JMP
 (CLEAR_DONE)
-    @LOOP
-    0;JMP
-
+@LOOP
+0;JMP
 (DRAW)
-   // put bitmap location value in R12
-	// put code return address in R13
-	@SCREEN
-	D=A
-	@R12
-	AD=D+M
-	// row 4
-	@512 // A holds val
-	D=D+A // D = addr + val
-	A=D-A // A=addr + val - val = addr
-	M=A-D // RAM[addr]=-val
-	AD=A+1 // D holds addr
-	@63 // A holds val
-	D=D+A // D = addr + val
-	A=D-A // A=addr + val - val = addr
-	M=D-A // RAM[addr] = val
-	// row 5
-	D=A // D holds previous addr
-	@31
-	AD=D+A
-	@960 // A holds val
-	D=D+A // D = addr + val
-	A=D-A // A=addr + val - val = addr
-	M=D-A // RAM[addr] = val
-	AD=A+1 // D holds addr
-	@224 // A holds val
-	D=D+A // D = addr + val
-	A=D-A // A=addr + val - val = addr
-	M=D-A // RAM[addr] = val
-	// row 6
-	D=A // D holds previous addr
-	@31
-	AD=D+A
-	@96 // A holds val
-	D=D+A // D = addr + val
-	A=D-A // A=addr + val - val = addr
-	M=D-A // RAM[addr] = val
-	AD=A+1 // D holds addr
-	@384 // A holds val
-	D=D+A // D = addr + val
-	A=D-A // A=addr + val - val = addr
-	M=D-A // RAM[addr] = val
-	// row 7
-	D=A // D holds previous addr
-	@31
-	AD=D+A
-	@48 // A holds val
-	D=D+A // D = addr + val
-	A=D-A // A=addr + val - val = addr
-	M=D-A // RAM[addr] = val
-	AD=A+1 // D holds addr
-	@256 // A holds val
-	D=D+A // D = addr + val
-	A=D-A // A=addr + val - val = addr
-	M=D-A // RAM[addr] = val
-	// row 8
-	D=A // D holds previous addr
-	@31
-	AD=D+A
-	@24 // A holds val
-	D=D+A // D = addr + val
-	A=D-A // A=addr + val - val = addr
-	M=D-A // RAM[addr] = val
-	AD=A+1 // D holds addr
-	@256 // A holds val
-	D=D+A // D = addr + val
-	A=D-A // A=addr + val - val = addr
-	M=D-A // RAM[addr] = val
-	// row 9
-	D=A // D holds previous addr
-	@31
-	AD=D+A
-	@8 // A holds val
-	D=D+A // D = addr + val
-	A=D-A // A=addr + val - val = addr
-	M=D-A // RAM[addr] = val
-	AD=A+1 // D holds addr
-	@8188 // A holds val
-	D=D+A // D = addr + val
-	A=D-A // A=addr + val - val = addr
-	M=D-A // RAM[addr] = val
-	// row 10
-	D=A // D holds previous addr
-	@31
-	AD=D+A
-	@8 // A holds val
-	D=D+A // D = addr + val
-	A=D-A // A=addr + val - val = addr
-	M=D-A // RAM[addr] = val
-	AD=A+1 // D holds addr
-	@8194 // A holds val
-	D=D+A // D = addr + val
-	A=D-A // A=addr + val - val = addr
-	M=D-A // RAM[addr] = val
-	// row 11
-	D=A // D holds previous addr
-	@31
-	AD=D+A
-	@14 // A holds val
-	D=D+A // D = addr + val
-	A=D-A // A=addr + val - val = addr
-	M=D-A // RAM[addr] = val
-	AD=A+1 // D holds addr
-	@8194 // A holds val
-	D=D+A // D = addr + val
-	A=D-A // A=addr + val - val = addr
-	M=D-A // RAM[addr] = val
-	// row 12
-	D=A // D holds previous addr
-	@31
-	AD=D+A
-	@11 // A holds val
-	D=D+A // D = addr + val
-	A=D-A // A=addr + val - val = addr
-	M=D-A // RAM[addr] = val
-	AD=A+1 // D holds addr
-	@8194 // A holds val
-	D=D+A // D = addr + val
-	A=D-A // A=addr + val - val = addr
-	M=D-A // RAM[addr] = val
-	// row 13
-	D=A // D holds previous addr
-	@31
-	AD=D+A
-	@9 // A holds val
-	D=D+A // D = addr + val
-	A=D-A // A=addr + val - val = addr
-	M=D-A // RAM[addr] = val
-	AD=A+1 // D holds addr
-	@8194 // A holds val
-	D=D+A // D = addr + val
-	A=D-A // A=addr + val - val = addr
-	M=D-A // RAM[addr] = val
-	// row 14
-	D=A // D holds previous addr
-	@31
-	AD=D+A
-	@9 // A holds val
-	D=D+A // D = addr + val
-	A=D-A // A=addr + val - val = addr
-	M=D-A // RAM[addr] = val
-	AD=A+1 // D holds addr
-	@8188 // A holds val
-	D=D+A // D = addr + val
-	A=D-A // A=addr + val - val = addr
-	M=D-A // RAM[addr] = val
-	// row 15
-	D=A // D holds previous addr
-	@31
-	AD=D+A
-	@9 // A holds val
-	D=D+A // D = addr + val
-	A=D-A // A=addr + val - val = addr
-	M=D-A // RAM[addr] = val
-	AD=A+1 // D holds addr
-	@1024 // A holds val
-	D=D+A // D = addr + val
-	A=D-A // A=addr + val - val = addr
-	M=D-A // RAM[addr] = val
-	// row 16
-	D=A // D holds previous addr
-	@31
-	AD=D+A
-	@9 // A holds val
-	D=D+A // D = addr + val
-	A=D-A // A=addr + val - val = addr
-	M=D-A // RAM[addr] = val
-	AD=A+1 // D holds addr
-	@1024 // A holds val
-	D=D+A // D = addr + val
-	A=D-A // A=addr + val - val = addr
-	M=D-A // RAM[addr] = val
-	// row 17
-	D=A // D holds previous addr
-	@31
-	AD=D+A
-	@9 // A holds val
-	D=D+A // D = addr + val
-	A=D-A // A=addr + val - val = addr
-	M=D-A // RAM[addr] = val
-	AD=A+1 // D holds addr
-	@1024 // A holds val
-	D=D+A // D = addr + val
-	A=D-A // A=addr + val - val = addr
-	M=D-A // RAM[addr] = val
-	// row 18
-	D=A // D holds previous addr
-	@31
-	AD=D+A
-	@9 // A holds val
-	D=D+A // D = addr + val
-	A=D-A // A=addr + val - val = addr
-	M=D-A // RAM[addr] = val
-	AD=A+1 // D holds addr
-	@1024 // A holds val
-	D=D+A // D = addr + val
-	A=D-A // A=addr + val - val = addr
-	M=D-A // RAM[addr] = val
-	// row 19
-	D=A // D holds previous addr
-	@31
-	AD=D+A
-	@9 // A holds val
-	D=D+A // D = addr + val
-	A=D-A // A=addr + val - val = addr
-	M=D-A // RAM[addr] = val
-	AD=A+1 // D holds addr
-	@1024 // A holds val
-	D=D+A // D = addr + val
-	A=D-A // A=addr + val - val = addr
-	M=D-A // RAM[addr] = val
-	// row 20
-	D=A // D holds previous addr
-	@31
-	AD=D+A
-	@9 // A holds val
-	D=D+A // D = addr + val
-	A=D-A // A=addr + val - val = addr
-	M=D-A // RAM[addr] = val
-	AD=A+1 // D holds addr
-	@1024 // A holds val
-	D=D+A // D = addr + val
-	A=D-A // A=addr + val - val = addr
-	M=D-A // RAM[addr] = val
-	// row 21
-	D=A // D holds previous addr
-	@31
-	AD=D+A
-	@15 // A holds val
-	D=D+A // D = addr + val
-	A=D-A // A=addr + val - val = addr
-	M=D-A // RAM[addr] = val
-	AD=A+1 // D holds addr
-	@1024 // A holds val
-	D=D+A // D = addr + val
-	A=D-A // A=addr + val - val = addr
-	M=D-A // RAM[addr] = val
-	// row 22
-	D=A // D holds previous addr
-	@31
-	AD=D+A
-	@8 // A holds val
-	D=D+A // D = addr + val
-	A=D-A // A=addr + val - val = addr
-	M=D-A // RAM[addr] = val
-	AD=A+1 // D holds addr
-	@1024 // A holds val
-	D=D+A // D = addr + val
-	A=D-A // A=addr + val - val = addr
-	M=D-A // RAM[addr] = val
-	// row 23
-	D=A // D holds previous addr
-	@31
-	AD=D+A
-	@16392 // A holds val
-	D=D+A // D = addr + val
-	A=D-A // A=addr + val - val = addr
-	M=D-A // RAM[addr] = val
-	AD=A+1 // D holds addr
-	@1024 // A holds val
-	D=D+A // D = addr + val
-	A=D-A // A=addr + val - val = addr
-	M=D-A // RAM[addr] = val
-	// row 24
-	D=A // D holds previous addr
-	@31
-	AD=D+A
-	@16392 // A holds val
-	D=D+A // D = addr + val
-	A=D-A // A=addr + val - val = addr
-	M=D-A // RAM[addr] = val
-	AD=A+1 // D holds addr
-	@1024 // A holds val
-	D=D+A // D = addr + val
-	A=D-A // A=addr + val - val = addr
-	M=D-A // RAM[addr] = val
-	// row 25
-	D=A // D holds previous addr
-	@31
-	AD=D+A
-	@16392 // A holds val
-	D=D+A // D = addr + val
-	A=D-A // A=addr + val - val = addr
-	M=D-A // RAM[addr] = val
-	AD=A+1 // D holds addr
-	@1024 // A holds val
-	D=D+A // D = addr + val
-	A=D-A // A=addr + val - val = addr
-	M=D-A // RAM[addr] = val
-	// row 26
-	D=A // D holds previous addr
-	@31
-	AD=D+A
-	@16392 // A holds val
-	D=D+A // D = addr + val
-	A=D-A // A=addr + val - val = addr
-	M=D-A // RAM[addr] = val
-	AD=A+1 // D holds addr
-	@1024 // A holds val
-	D=D+A // D = addr + val
-	A=D-A // A=addr + val - val = addr
-	M=D-A // RAM[addr] = val
-	// row 27
-	D=A // D holds previous addr
-	@31
-	AD=D+A
-	@16392 // A holds val
-	D=D+A // D = addr + val
-	A=D-A // A=addr + val - val = addr
-	M=D-A // RAM[addr] = val
-	AD=A+1 // D holds addr
-	@1024 // A holds val
-	D=D+A // D = addr + val
-	A=D-A // A=addr + val - val = addr
-	M=D-A // RAM[addr] = val
-	// row 28
-	D=A // D holds previous addr
-	@31
-	AD=D+A
-	@16392 // A holds val
-	D=D+A // D = addr + val
-	A=D-A // A=addr + val - val = addr
-	M=D-A // RAM[addr] = val
-	AD=A+1 // D holds addr
-	@1024 // A holds val
-	D=D+A // D = addr + val
-	A=D-A // A=addr + val - val = addr
-	M=D-A // RAM[addr] = val
-	// row 29
-	D=A // D holds previous addr
-	@31
-	AD=D+A
-	@16392 // A holds val
-	D=D+A // D = addr + val
-	A=D-A // A=addr + val - val = addr
-	M=D-A // RAM[addr] = val
-	AD=A+1 // D holds addr
-	@1024 // A holds val
-	D=D+A // D = addr + val
-	A=D-A // A=addr + val - val = addr
-	M=D-A // RAM[addr] = val
-	// row 30
-	D=A // D holds previous addr
-	@31
-	AD=D+A
-	@16392 // A holds val
-	D=D+A // D = addr + val
-	A=D-A // A=addr + val - val = addr
-	M=D-A // RAM[addr] = val
-	AD=A+1 // D holds addr
-	@1024 // A holds val
-	D=D+A // D = addr + val
-	A=D-A // A=addr + val - val = addr
-	M=D-A // RAM[addr] = val
-	// row 31
-	D=A // D holds previous addr
-	@31
-	AD=D+A
-	@16392 // A holds val
-	D=D+A // D = addr + val
-	A=D-A // A=addr + val - val = addr
-	M=D-A // RAM[addr] = val
-	AD=A+1 // D holds addr
-	@1024 // A holds val
-	D=D+A // D = addr + val
-	A=D-A // A=addr + val - val = addr
-	M=D-A // RAM[addr] = val
-	// row 32
-	D=A // D holds previous addr
-	@31
-	AD=D+A
-	@16400 // A holds val
-	D=D+A // D = addr + val
-	A=D-A // A=addr + val - val = addr
-	M=A-D // RAM[addr]=-val
-	AD=A+1 // D holds addr
-	@1023 // A holds val
-	D=D+A // D = addr + val
-	A=D-A // A=addr + val - val = addr
-	M=D-A // RAM[addr] = val
-	// return
-	@R13
-	A=M
-	D;JMP
-
-    @LOOP
-    0;JMP
+@SCREEN
+D=A
+@R12
+M=D
+@512
+D=A
+@R12
+A=M
+M=D
+@R12
+D=M
+D=D+1
+@R12
+M=D
+@63
+D=A
+@R12
+A=M
+M=D
+@R12
+D=M
+D=D+1
+@R12
+M=D
+@960
+D=A
+@R12
+A=M
+M=D
+@R12
+D=M
+D=D+1
+@R12
+M=D
+@224
+D=A
+@R12
+A=M
+M=D
+@R12
+D=M
+D=D+1
+@R12
+M=D
+@96
+D=A
+@R12
+A=M
+M=D
+@R12
+D=M
+D=D+1
+@R12
+M=D
+@384
+D=A
+@R12
+A=M
+M=D
+@R12
+D=M
+D=D+1
+@R12
+M=D
+@48
+D=A
+@R12
+A=M
+M=D
+@R12
+D=M
+D=D+1
+@R12
+M=D
+@256
+D=A
+@R12
+A=M
+M=D
+@R12
+D=M
+D=D+1
+@R12
+M=D
+@24
+D=A
+@R12
+A=M
+M=D
+@R12
+D=M
+D=D+1
+@R12
+M=D
+@256
+D=A
+@R12
+A=M
+M=D
+@R12
+D=M
+D=D+1
+@R12
+M=D
+@8
+D=A
+@R12
+A=M
+M=D
+@R12
+D=M
+D=D+1
+@R12
+M=D
+@8188
+D=A
+@R12
+A=M
+M=D
+@R12
+D=M
+D=D+1
+@R12
+M=D
+@8
+D=A
+@R12
+A=M
+M=D
+@R12
+D=M
+D=D+1
+@R12
+M=D
+@8194
+D=A
+@R12
+A=M
+M=D
+@R12
+D=M
+D=D+1
+@R12
+M=D
+@14
+D=A
+@R12
+A=M
+M=D
+@R12
+D=M
+D=D+1
+@R12
+M=D
+@8194
+D=A
+@R12
+A=M
+M=D
+@R12
+D=M
+D=D+1
+@R12
+M=D
+@11
+D=A
+@R12
+A=M
+M=D
+@R12
+D=M
+D=D+1
+@R12
+M=D
+@8194
+D=A
+@R12
+A=M
+M=D
+@R12
+D=M
+D=D+1
+@R12
+M=D
+@9
+D=A
+@R12
+A=M
+M=D
+@R12
+D=M
+D=D+1
+@R12
+M=D
+@8194
+D=A
+@R12
+A=M
+M=D
+@R12
+D=M
+D=D+1
+@R12
+M=D
+@9
+D=A
+@R12
+A=M
+M=D
+@R12
+D=M
+D=D+1
+@R12
+M=D
+@8188
+D=A
+@R12
+A=M
+M=D
+@R12
+D=M
+D=D+1
+@R12
+M=D
+@9
+D=A
+@R12
+A=M
+M=D
+@R12
+D=M
+D=D+1
+@R12
+M=D
+@1024
+D=A
+@R12
+A=M
+M=D
+@R12
+D=M
+D=D+1
+@R12
+M=D
+@9
+D=A
+@R12
+A=M
+M=D
+@R12
+D=M
+D=D+1
+@R12
+M=D
+@1024
+D=A
+@R12
+A=M
+M=D
+@R12
+D=M
+D=D+1
+@R12
+M=D
+@9
+D=A
+@R12
+A=M
+M=D
+@R12
+D=M
+D=D+1
+@R12
+M=D
+@1024
+D=A
+@R12
+A=M
+M=D
+@R12
+D=M
+D=D+1
+@R12
+M=D
+@9
+D=A
+@R12
+A=M
+M=D
+@R12
+D=M
+D=D+1
+@R12
+M=D
+@1024
+D=A
+@R12
+A=M
+M=D
+@R12
+D=M
+D=D+1
+@R12
+M=D
+@9
+D=A
+@R12
+A=M
+M=D
+@R12
+D=M
+D=D+1
+@R12
+M=D
+@1024
+D=A
+@R12
+A=M
+M=D
+@R12
+D=M
+D=D+1
+@R12
+M=D
+@9
+D=A
+@R12
+A=M
+M=D
+@R12
+D=M
+D=D+1
+@R12
+M=D
+@1024
+D=A
+@R12
+A=M
+M=D
+@R12
+D=M
+D=D+1
+@R12
+M=D
+@15
+D=A
+@R12
+A=M
+M=D
+@R12
+D=M
+D=D+1
+@R12
+M=D
+@1024
+D=A
+@R12
+A=M
+M=D
+@R12
+D=M
+D=D+1
+@R12
+M=D
+@8
+D=A
+@R12
+A=M
+M=D
+@R12
+D=M
+D=D+1
+@R12
+M=D
+@1024
+D=A
+@R12
+A=M
+M=D
+@R12
+D=M
+D=D+1
+@R12
+M=D
+@16392
+D=A
+@R12
+A=M
+M=D
+@R12
+D=M
+D=D+1
+@R12
+M=D
+@1024
+D=A
+@R12
+A=M
+M=D
+@R12
+D=M
+D=D+1
+@R12
+M=D
+@16392
+D=A
+@R12
+A=M
+M=D
+@R12
+D=M
+D=D+1
+@R12
+M=D
+@1024
+D=A
+@R12
+A=M
+M=D
+@R12
+D=M
+D=D+1
+@R12
+M=D
+@16392
+D=A
+@R12
+A=M
+M=D
+@R12
+D=M
+D=D+1
+@R12
+M=D
+@1024
+D=A
+@R12
+A=M
+M=D
+@R12
+D=M
+D=D+1
+@R12
+M=D
+@16392
+D=A
+@R12
+A=M
+M=D
+@R12
+D=M
+D=D+1
+@R12
+M=D
+@1024
+D=A
+@R12
+A=M
+M=D
+@R12
+D=M
+D=D+1
+@R12
+M=D
+@16392
+D=A
+@R12
+A=M
+M=D
+@R12
+D=M
+D=D+1
+@R12
+M=D
+@1024
+D=A
+@R12
+A=M
+M=D
+@R12
+D=M
+D=D+1
+@R12
+M=D
+@16392
+D=A
+@R12
+A=M
+M=D
+@R12
+D=M
+D=D+1
+@R12
+M=D
+@1024
+D=A
+@R12
+A=M
+M=D
+@R12
+D=M
+D=D+1
+@R12
+M=D
+@16392
+D=A
+@R12
+A=M
+M=D
+@R12
+D=M
+D=D+1
+@R12
+M=D
+@LOOP
+0;JMP
